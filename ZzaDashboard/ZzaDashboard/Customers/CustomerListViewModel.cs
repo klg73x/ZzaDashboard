f2beea19c9acbd8409281200b4b3f37e3875d658
Kevin Gagnon <klg73@outlook.com>
2018-10-05 10:47:24 -0400

    The  Customer List view is set up more traditionally

---
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.Customers
+{
+    public class CustomerListViewModel
+    {
+        private ICustomersRepository _repo = new CustomersRepository();
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(
+                new System.Windows.DependencyObject())) return; //we use this line so that the call to data won't execute in the designer
+            Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result); //this .Result is needed because we are calling this syncronously even though it is an async call
+        }
+    }
+}
