0811345956513aefdc336462d92114d2aa5eae9d
Kevin Gagnon <klg73@outlook.com>
2018-10-15 09:48:32 -0400

    Delete Command Added with Can Execute property

--- ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
+++ ZzaDashboard/ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Zza.Data;
 using ZzaDashboard.Services;
 
@@ -12,15 +8,49 @@ namespace ZzaDashboard.Customers
 {
     public class CustomerListViewModel
     {
+        private ObservableCollection<Customer> _customers;
         private ICustomersRepository _repo = new CustomersRepository();
+        public RelayCommand DeleteCommand { get; private set; }
+        public ObservableCollection<Customer> Customers
+        {
+            get
+            {
+                return _customers;
+            }
+            set
+            {
+                _customers = value;
+            }
+        }
 
         public CustomerListViewModel()
         {
             if (DesignerProperties.GetIsInDesignMode(
                 new System.Windows.DependencyObject())) return; //we use this line so that the call to data won't execute in the designer
             Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result); //this .Result is needed because we are calling this syncronously even though it is an async call
+            DeleteCommand = new RelayCommand(OnDelete, CanDelete);
+        }
+        private Customer _selectedCustomer;
+        public Customer SelectedCustomer
+        {
+            get
+            {
+                return _selectedCustomer;
+            }
+            set
+            {
+                _selectedCustomer = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+        private bool CanDelete()
+        {
+            return SelectedCustomer != null;
         }
 
-        public ObservableCollection<Customer> Customers { get; set; }
+        private void OnDelete()
+        {
+            Customers.Remove(SelectedCustomer);
+        }
     }
 }
